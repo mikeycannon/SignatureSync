4c7a4986e786455a91b0835507da4f279e3c63a1
Mikey Cannon <mikeycandesign@gmail.com>
2025-07-04 13:41:21 +0100

    Update template-editor.tsx

--- client/src/pages/template-editor.tsx
+++ client/src/pages/template-editor.tsx
@@ -1,5 +1,17 @@
-import { SignatureEditor } from '@/components/signature-editor';
+import { Sidebar } from "@/components/layout/sidebar";
+import { TopBar } from "@/components/layout/topbar";
+import { SignatureEditor } from "@/components/signature-editor";
 
 export default function TemplateEditor() {
-  return <SignatureEditor onSave={() => {}} onCancel={() => {}} />;
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <TopBar title="Signature Editor" />
+        <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
+          <SignatureEditor onSave={() => {}} onCancel={() => {}} />
+        </main>
+      </div>
+    </div>
+  );
 }
